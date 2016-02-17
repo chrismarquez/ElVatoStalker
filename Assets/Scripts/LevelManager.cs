dde05699fb2fc275d6d1c8b60cd3ed41b3d78a2f
chrismarquez <a01225198@itesm.mx>
2016-02-17 15:41:59 -0600

    First Partial Advance

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,14 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour {
 
 	// Use this for initialization
 	public void LoadScene(string name){
-		Application.LoadLevel (name);
+        Application.LoadLevel(name);
 	}
 
 	public void QuitGame(){
-		Application.Quit ();
-	}
+        Application.Quit();
+    }
 }
